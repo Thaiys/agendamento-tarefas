fea06a88b5ec180f973b9f0257bda425a47a45d0
Thaiys <tmendonca70@gmail.com>
2025-04-23 21:44:33 -0400

    iniciada implementacao de tarefas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,43 @@
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
+import TarefaItem from './TarefaItem';
 
-export default function Home(){
+export default function Home() {
     return (
-        <View>
-            <Text>Home Page</Text>
+        <View style= {styles.container}>
+            <View style= {styles.cabecalho}>
+                <Text style= {styles.titulo}> ABRIL / 2025</Text>
+                <View style= {styles.icone}></View>
+            </View>
+            <View style={styles.body}>
+                <TarefaItem/>
+
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    cabecalho: {
+        backgroundColor: 'blue',
+        width: '100%',
+        height: 60,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+    titulo: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+    icone:{
+        backgroundColor:'gray',
+        width:30,
+        height:30,
+        borderRadius: 15,
+        position: 'absolute',
+        right:15
+    }
+});
